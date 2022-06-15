c0f4f8cab0ef427d960bf2b82b1244059bb7c0c4
Vigen Simonyan <vigen.simonyan@ihsmarkit.com>
2022-06-15 18:19:46 +0400

    finalizedAddingPage

--- src/App.js
+++ src/App.js
@@ -2,68 +2,31 @@ import './App.css';
 import CreateShopItem from './components/CreateShopItem.coponent';
 import ShopItems from './components/ShopItems.component';
 import Button from '@mui/material/Button';
-import ShopListItem from './components/ShopListItem.component'
-import shortid from 'shortid'
+import { useSelector, useDispatch } from 'react-redux'
+import { addShopItems, clearShopitemsToAdd } from './components/createSlice';
 
 function App() {
-  let item = [
-    {
-      id: shortid.generate(),
-      Name: "banan1",
-      Price: "1001",
-      Weight: 101,
-      StartDate: new Date(1),
-      EndDate: new Date(3)
-    },
-    {
-      id: shortid.generate(),
-      Name: "banan2",
-      Price: "1002",
-      Weight: 102,
-      StartDate: new Date(2),
-      EndDate: new Date(5)
-    },
-    {
-      id: shortid.generate(),
-      Name: "bana3n",
-      Price: "1003",
-      Weight: 104,
-      StartDate: new Date(4),
-      EndDate: new Date(7)
-    }
-    ,
-    {
-      id: shortid.generate(),
-      Name: "bana3n",
-      Price: "1003",
-      Weight: 104,
-      StartDate: new Date(4),
-      EndDate: new Date(7)
-    }
-    ,
-    {
-      id: shortid.generate(),
-      Name: "bana3n",
-      Price: "1003",
-      Weight: 104,
-      StartDate: new Date(4),
-      EndDate: new Date(7)
-    }
-  ]
+  const items = useSelector((state) => state.shopItems)
+  const dispatch = useDispatch();
 
+  function saveItems() {
+    if (items.shopItemsToAdd){
+      dispatch(addShopItems(items.shopItemsToAdd));
+      dispatch(clearShopitemsToAdd());
+    }
+  }
   return (
     <div className='container'>
       <div className='create-container'>
         <div className='header-container'>
           <h3 className='title'>Create Shop Items</h3>
-          <Button variant="contained">Save items</Button>
+          <Button variant="contained" onClick={saveItems}>Save items</Button>
         </div>
         <CreateShopItem />
       </div>
       <div className='created-items-container'>
-        <ShopItems shopItems = {item}/>
+        <ShopItems/>
       </div>
-
     </div>
   );
 }
